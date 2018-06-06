3a07b1872aa1e81b33c90c169dd24af5be713887
Cézar Augusto Ferreira <cezaraf@gmail.com>
2018-06-06 18:41:21 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tcm-root',
+  selector: 'app-tcm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 
-  dataNascimento = new Date()
+  multipleUpload = true;
+
+  disabled = false;
+
+  collapsed = false;
+
+  toggleable = false;
+
+  uploadAuto = true;
+
+  tableSignedFiles = 'Arquivos assinados';
+
+  tableFilesToSign = 'Arquivos a serem assinados';
+
+  chooseLabel = 'Selecionar documentos para assinatura';
+
+  cancelLabel = 'Cancelar Envio';
+
+  uploadLabel = 'Enviar documentos para assinatura';
+
+  accept = '*/*';
+
+  header = 'Assinador TCM';
+
+  exibirModalDeleteFile = false;
+
+  canCoSign = false;
+
+  canDeleteFiles = false;
+
+  canViewSigners = true;
+
+  canDownloadFiles = true;
+
+  uploadEnabled = true;
+
+  eventoDeleteFile = {};
+
+  arquivos = [
+    {id: 'd8f3705e-3939-4862-a8fa-be0b127b9405'}
+  ];
+
+  dataNascimento = new Date();
+
+  deleteFile($event) {
+
+    this.eventoDeleteFile = JSON.stringify($event, (key, value) => {
+
+      if (typeof value === 'function') { return 'function() { ... }'; } else { return value; }
+
+    }, '  ');
+
+    this.exibirModalDeleteFile = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,69 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { CalendarModule, DataTableModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
-import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
-import { HomeComponent } from './home/home.component';
-import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
-import { RouterModule, Route } from '@angular/router';
-import { PessoaService } from './cadastro-pessoa/pessoa.service';
-import { PessoaPipe } from './cadastro-pessoa/pessoa.pipe';
-import { PessoaOrdenacaoPipe } from './cadastro-pessoa/pessoa-ordenacao.pipe';
-import { CadastroPessoaPrimengComponent } from './cadastro-pessoa-primeng/cadastro-pessoa-primeng.component';
-
-const rotasAplicacao: Route[] = [
-
-  {
-    path: '',
-
-    component: HomeComponent,
-
-    data: { titulo: 'PÁGINA INICIAL' }
-  },
-
-  {
-    path: 'pagina-cadastro-pessoa',
-
-    component: CadastroPessoaComponent,
-
-    data: { titulo: "PAGINA DE CADASTRO DE PESSOA" }
-  },
-
-  {
-    path: 'pagina-cadastro-pessoa-primeng',
-
-    component: CadastroPessoaPrimengComponent,
-
-    data: { titulo: "PAGINA DE CADASTRO DE PESSOA" }
-  }
-
-]
+import { BaseModule, BryModule } from 'tcm-base-angular2';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuComponenteComponent,
-    HomeComponent,
-    CadastroPessoaComponent,
-    PessoaPipe,
-    PessoaOrdenacaoPipe,
-    CadastroPessoaPrimengComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    CalendarModule,
-    DataTableModule,
-    HttpModule,
-    RouterModule.forRoot(rotasAplicacao, { useHash: true })
-  ],
-  providers: [
-    PessoaService
+    BaseModule,
+    BryModule.forRoot({
+      servidorDeAssinatura: 'http://10.10.110.227:8080',
+      tokenPermissao: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBRDg2OTM5MTZGRTk0NjAyOTY4NkI3MzA5MTRDQURCNiIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTUyODIxODQ4ODA3MywiZXhwIjoxNTU5NzU0NDg4fQ.EuU9RFxtK20wAKN-YFhKB2v-xQ5XEKlDNflFLEhYFKqVic6TvPL3tft1Jp72m85Nagn1-8YM9ZsO7_bgEJLQNg',
+      sistema: 'esiproc'
+    })
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
