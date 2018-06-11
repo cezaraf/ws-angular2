2d4ca4ed1992298070989c0e93a334bde0565d43
Cézar Augusto Ferreira <cezaraf@gmail.com>
2018-06-11 18:11:37 -0300

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
 
     sistema: 'assinador-exemplo-frontend',
 
-    servidorDeAssinatura: 'http://homologa.tcm.go.gov.br/assinador',
+    servidorDeAssinatura: 'https://homologa.tcm.go.gov.br/assinador',
 
     tokenPermissao: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBRDg2OTM5MTZGRTk0NjAyOTY4NkI3MzA5MTRDQURCNiIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTUyODIxODQ4ODA3MywiZXhwIjoxNTU5NzU0NDg4fQ.EuU9RFxtK20wAKN-YFhKB2v-xQ5XEKlDNflFLEhYFKqVic6TvPL3tft1Jp72m85Nagn1-8YM9ZsO7_bgEJLQNg'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
 
     sistema: 'assinador-exemplo-frontend',
 
-    servidorDeAssinatura: 'http://localhost:8080',
+    servidorDeAssinatura: 'https://localhost:8443',
 
     tokenPermissao: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBRDg2OTM5MTZGRTk0NjAyOTY4NkI3MzA5MTRDQURCNiIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTUyODIxODQ4ODA3MywiZXhwIjoxNTU5NzU0NDg4fQ.EuU9RFxtK20wAKN-YFhKB2v-xQ5XEKlDNflFLEhYFKqVic6TvPL3tft1Jp72m85Nagn1-8YM9ZsO7_bgEJLQNg'
   }
